ebf00112b5b4175e18890a1507725fdc12eec3d1
Fernando Guirao <fernando@bueninvento.es>
2013-09-04 12:33:18 +0200

    Inicializador html

--- templates/persistent/header.php
+++ templates/persistent/header.php
@@ -10,7 +10,16 @@
     <title></title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width">
+    <META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">
 
+    <!-- Iconos -->
+    <link rel="shortcut icon" type="image/x-icon" href="media/img/icons/favicon.ico">
+    <link rel="apple-touch-icon" href="media/img/icons/apple-touch-icon.png"/>
+    <link rel="apple-touch-icon" sizes="57x57" href="media/img/icons/apple-touch-icon-57x57.png"/>
+    <link rel="apple-touch-icon" sizes="72x72" href="media/img/icons/apple-touch-icon-72x72.png"/>
+    <link rel="apple-touch-icon" sizes="114x114" href="media/img/icons/apple-touch-icon-114x114.png"/>
+    <link rel="apple-touch-icon" sizes="144x144" href="media/img/icons/apple-touch-icon-144x144.png"/>
+    
     <!-- Styles -->
     <link rel="stylesheet" href="static/css/bootstrap.min.css">
     <style>
