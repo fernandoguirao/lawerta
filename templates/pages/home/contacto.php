5c004a42c352ab0e357a4d317c5c775790934aec
Fernando Guirao <fernando@bueninvento.es>
2013-11-06 21:39:52 +0100

    Contacto terminado

---
+++ templates/pages/home/contacto.php
@@ -0,0 +1,29 @@
+<div id="single" class="contacto">
+  <div class="proyecto container" style="display:none;">
+    <div class="row info-col">
+      <div class="col-md-6">
+        <div class="info-project row">
+          <p class="big">Hi there!</p>
+          <p class="big">Hope you liked my work.</p>
+          <p class="big">You can reach me here:</p>
+          <br>
+          <p class="big"><a href="mailto:lawerta@lawerta.com"><b>lawerta@lawerta.com</b></a></p>
+          <br>
+          <p class="big">For new business please contact:</p>
+          <p class="big"><img src="/static/img/mushroom.png" alt=""></p>
+          <p class="big"><b>The Mushroom Company</b></p>
+          <p class="big">Luis Delgado / Guillermina Trejo</p>
+          <p class="big">619.382.054 / 661.815.404
+          <p class="big"><a href="mailto:info@mushroom.es">info@mushroom.es</a></p>
+          <p class="big"><a href="http://www.mushroom.es" target="_blank"><b>www.mushroom.es</b></a></p>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-offset-6 col-md-6 slides">
+      <div class="slide">
+        <img src="/static/img/contacto.jpg" alt="">
+      </div>
+    </div>
+  </div>
+</div>
+
